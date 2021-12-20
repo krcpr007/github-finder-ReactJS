1c55f54e7f7daad03b1034f4542bea5830915ab3
Rajan kumar <krcpr007@gmail.com>
2021-12-20 13:15:04 +0530

    rapping App by context API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GithubProvider } from "./components/context/GithubContext";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <GithubProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GithubProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
