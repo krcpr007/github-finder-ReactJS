d4ec1640f18c4d79d1f93e52e0db9d31f012707f
Rajan kumar <krcpr007@gmail.com>
2021-12-20 14:43:31 +0530

    Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import User from "./User";
+import Search from './Search'
+function Home() {
+    return (
+        <div>
+            {/* Search component */}
+            <Search/>
+            <User/>
+            
+            
+        </div>
+    )
+}
+
+export default Home
