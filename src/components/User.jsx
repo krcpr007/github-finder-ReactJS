991bfd6b089c5e0fa6c9fcecffbdc7cb1857baf9
Rajan kumar <krcpr007@gmail.com>
2021-12-20 13:14:02 +0530

    Fetching from context api

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,34 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import Spinner from "./Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "./context/GithubContext";
 function User() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
   useEffect(() => {
     fetchUsers();
   }, []);
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_API_KEY}`,
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    setUsers(data);
-    setLoading(false);
-  };
-  if(!loading){
 
-      return (
-        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-            {users.map((user)=>{
-                return <UserItem key={user.id} user={user}/>
-            })}
-        </div>
-      );
-  }else{
-      return <Spinner/>
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => {
+          return <UserItem key={user.id} user={user} />;
+        })}
+      </div>
+    );
+  } else {
+    return <Spinner />;
   }
 }
 
