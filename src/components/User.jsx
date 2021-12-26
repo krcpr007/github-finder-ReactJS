27079c3ec193ac17c3afb511fd4c857702151e16
Rajan kumar <krcpr007@gmail.com>
2021-12-26 21:32:34 +0530

    spinner updated

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -53,7 +53,7 @@ function User() {
 
         <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
           <div className="custom-card-image mb-6 md:mb-0">
-            <div className="rounded-lg shadow-xl card image-full mx-7  w-60">
+            <div className="rounded-lg shadow-xl card image-full mx-7">
               {/*image-full */}
               <figure>
                 <img src={avatar_url} alt="" />
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -17,7 +17,13 @@ function Users() {
       </div>
     );
   } else {
-    return <Spinner />;
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => {
+          return <Spinner key={user.id} />;
+        })}
+      </div>
+    );
   }
 }
 
