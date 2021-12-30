738fd61e5ef6bde704badc66deaaf4ea010cc9b1
Rajan kumar <krcpr007@gmail.com>
2021-12-30 22:41:02 +0530

    alert context added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
 import GithubContext from "./context/GithubContext";
+import AlertContext from "./context/AlertContext";
 function Search() {
+  const { setAlert } = useContext(AlertContext);
   const { users, searchResults, clearResults } = useContext(GithubContext);
   const [text, setText] = useState("");
   const handleChange = (e) => {
@@ -9,7 +11,8 @@ function Search() {
   const hadleSubmit = (e) => {
     e.preventDefault();
     if (text.length === 0) {
-      alert("Please enter something");
+      // alert("Please enter something");
+      setAlert("Please Enter Something", "error");
     } else {
       searchResults(text);
       setText("");
@@ -19,33 +22,33 @@ function Search() {
     clearResults();
   };
   return (
-      <div className="grid gird-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
-        <div>
-          <form onSubmit={hadleSubmit}>
-            <div className="from-control">
-              <div className="relative ">
-                <input 
-                  type="text"
-                  className="w-full  pr-40 bg-gray-200 input text-black border-purple-500"
-                  placeholder="Search"
-                  value={text}
-                  onChange={handleChange}
-                />
-                <button className="absolute top-0 right-0 rounded-l-none w-36 btn border-purple-500 ">
-                  Go
-                </button>
-              </div>
+    <div className="grid gird-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+      <div>
+        <form onSubmit={hadleSubmit}>
+          <div className="from-control">
+            <div className="relative ">
+              <input
+                type="text"
+                className="w-full  pr-40 bg-gray-200 input text-black border-purple-500"
+                placeholder="Search"
+                value={text}
+                onChange={handleChange}
+              />
+              <button className="absolute top-0 right-0 rounded-l-none w-36 btn border-purple-500 ">
+                Go
+              </button>
             </div>
-          </form>
-        </div>
-        {users.length > 0 && (
-          <div>
-            <button className="btn btn-ghost -btn-lg" onClick={handleClear}>
-              Clear
-            </button>
           </div>
-        )}
+        </form>
       </div>
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost -btn-lg" onClick={handleClear}>
+            Clear
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
